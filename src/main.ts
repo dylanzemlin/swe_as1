19cc96e6a6aca78117538a5f471d68e99a6e1057
Dylan Zemlin <dylansmrw@gmail.com>
2022-09-29 21:58:15 -0500

    Add relative path function

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 
+/**
+ * Creates a path relative to the source directory
+ * @param p  The file name.
+ * @returns  The path relative to source directory.
+ */
+const relative = (p: string) => path.join(__dirname, "../src", p);
+
 // Create express application using json parsing and a public folder
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Read in favs.json
 let data: any[] | undefined = undefined;
-const favs = fs.readFileSync(`${__dirname}/favs.json`, "utf8");
+const favs = fs.readFileSync(relative("favs.json"), "utf8");
 try {
   data = JSON.parse(favs);
 } catch (e) {
@@ -18,7 +25,7 @@ try {
 
 // Serve index.html on the route '/'
 app.get("/", (_, res) => {
-	return res.sendFile(path.join(__dirname, "..", "src/index.html"));
+  return res.sendFile(relative("index.html"));
 });
 
 /**
@@ -27,13 +34,13 @@ app.get("/", (_, res) => {
  * 	- 200: A list of tweets
  */
 app.get("/tweets", (_, res) => {
-	return res.json(data?.map((tweet: any) => {
-		return {
-			id: tweet.id,
-			created_at: tweet.created_at,
-			text: tweet.text
-		}
-	}))
+  return res.json(data?.map((tweet: any) => {
+    return {
+      id: tweet.id,
+      created_at: tweet.created_at,
+      text: tweet.text
+    }
+  }))
 });
 
 /**
@@ -43,17 +50,17 @@ app.get("/tweets", (_, res) => {
  *  - 404: no_tweet_found error
  */
 app.get("/tweets/:id", (req, res) => {
-	if (data?.findIndex(tweet => tweet.id == req.params.id) == -1) {
-		return res.status(404).json({ error: "no_tweet_found", message: "No tweet found with that id" });
-	}
+  if (data?.findIndex(tweet => tweet.id == req.params.id) == -1) {
+    return res.status(404).json({ error: "no_tweet_found", message: "No tweet found with that id" });
+  }
 
-	return res.json(data?.map(tweet => {
-		return {
-			id: tweet.id,
-			created_at: tweet.created_at,
-			text: tweet.text
-		}
-	}).find(x => x.id == req.params.id)); // do a non-typed compare to avoid int == string comparision
+  return res.json(data?.map(tweet => {
+    return {
+      id: tweet.id,
+      created_at: tweet.created_at,
+      text: tweet.text
+    }
+  }).find(x => x.id == req.params.id)); // do a non-typed compare to avoid int == string comparision
 });
 
 /**
@@ -62,28 +69,28 @@ app.get("/tweets/:id", (req, res) => {
  *  - 200: The list of users
  */
 app.get("/users", (_, res) => {
-	// Map each tweet to acquire a list of users, 
-	// then filter out users to ensure there are no duplicates
+  // Map each tweet to acquire a list of users, 
+  // then filter out users to ensure there are no duplicates
 
   // Undefined users had to be filtered out becuase 
   // no user is inserted when a new tweet is created
-	return res.json(data?.map((tweet: any) => {
-		if (tweet.user == null) {
-			return undefined;
-		}
+  return res.json(data?.map((tweet: any) => {
+    if (tweet.user == null) {
+      return undefined;
+    }
 
-		return {
-			id: tweet.user?.id,
-			name: tweet.user?.name,
-			screen_name: tweet.user?.screen_name
-		}
-	}).filter((user: any, index: number, self: any) => {
-    if(self == null || user == null) {
+    return {
+      id: tweet.user?.id,
+      name: tweet.user?.name,
+      screen_name: tweet.user?.screen_name
+    }
+  }).filter((user: any, index: number, self: any) => {
+    if (self == null || user == null) {
       return false;
     }
 
-		return self.findIndex((u: any) => u?.id === user?.id) === index;
-	}));
+    return self.findIndex((u: any) => u?.id === user?.id) === index;
+  }));
 });
 
 /**
@@ -93,21 +100,21 @@ app.get("/users", (_, res) => {
  *  - 400: The body was invalid (no id or text)
  */
 app.post("/tweets", (req, res) => {
-	if (!req.body.text || !req.body.id) {
-		return res.status(400).json({ error: "invalid_body" });
-	}
+  if (!req.body.text || !req.body.id) {
+    return res.status(400).json({ error: "invalid_body" });
+  }
 
-	if (data?.findIndex(tweet => tweet.id == req.body.id) != -1) {
-		return res.status(400).json({ error: "invalid_id" });
-	}
+  if (data?.findIndex(tweet => tweet.id == req.body.id) != -1) {
+    return res.status(400).json({ error: "invalid_id" });
+  }
 
-	data?.push({
-		text: req.body.text,
-		id: req.body.id,
-		created_at: new Date().toISOString()
-	});
+  data?.push({
+    text: req.body.text,
+    id: req.body.id,
+    created_at: new Date().toISOString()
+  });
 
-	return res.status(201).end();
+  return res.status(201).end();
 });
 
 /**
@@ -117,25 +124,25 @@ app.post("/tweets", (req, res) => {
  * 	- 400: Invalid body (no new_name field)
  */
 app.patch("/users", (req, res) => {
-	if (!req.body.screen_name || !req.body.name) {
-		return res.status(400).json({ error: "invalid_body" });
-	}
+  if (!req.body.screen_name || !req.body.name) {
+    return res.status(400).json({ error: "invalid_body" });
+  }
 
-	if(!data?.some(tweet => tweet.user?.name == req.body.name)) {
-		return res.status(404).json({ error: "no_user_found" });
-	}
+  if (!data?.some(tweet => tweet.user?.name == req.body.name)) {
+    return res.status(404).json({ error: "no_user_found" });
+  }
 
-	data?.forEach((tweet: any) => {
-    if(tweet.user == null) {
+  data?.forEach((tweet: any) => {
+    if (tweet.user == null) {
       return;
     }
 
-		if (tweet.user.name == req.body.name) {
-			tweet.user.screen_name = req.body.screen_name;
-		}
-	});
+    if (tweet.user.name == req.body.name) {
+      tweet.user.screen_name = req.body.screen_name;
+    }
+  });
 
-	return res.status(204).end();
+  return res.status(204).end();
 });
 
 /**
@@ -145,16 +152,16 @@ app.patch("/users", (req, res) => {
  *  - 404: no_tweet_found error
  */
 app.delete("/tweets/:id", (req, res) => {
-	if (data?.findIndex(tweet => tweet.id == req.params.id) == -1) {
-		return res.status(404).json({ error: "no_tweet_found" });
-	}
+  if (data?.findIndex(tweet => tweet.id == req.params.id) == -1) {
+    return res.status(404).json({ error: "no_tweet_found" });
+  }
 
-	const tweet = data?.find(tweet => tweet.id == req.params.id)
-	data = data?.filter(tweet => tweet.id != req.params.id);
-	return res.json(tweet);
+  const tweet = data?.find(tweet => tweet.id == req.params.id)
+  data = data?.filter(tweet => tweet.id != req.params.id);
+  return res.json(tweet);
 })
 
 // Listen on port 3000 and log the url
 app.listen(3000, () => {
-	console.log(`Listening at: http://127.0.0.1:3000/`);
+  console.log(`Listening at: http://127.0.0.1:3000/`);
 });
